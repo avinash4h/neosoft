9f4daf8c071173da420f3d3c4f3fd0bcea9922f0
APKAX3VNMUIDDRYF7H5P <avinash.chopade@neosoftmail>
2023-07-31 12:41:26 +0530

    Interview questons TCL

--- java programs/MapOp.java
+++ java programs/MapOp.java
@@ -1,3 +1,30 @@
+
+/*
+Write a java program to segregate vegetables and fruits from a basket and tag prices as per current market value.
+ 
+    Basket = {"FRT", "Apple"},{"VG", "Onion"},{"VG", "Potato"},{"VG", "Chillies"},{"FRT", "Mango"},{"FRT", "Banana"}
+ 
+    Price/kg = {"200", "Apple"},
+               {"20", "Onion"},
+               {"20", "Potato"},
+               {"80", "Chillies"},
+               {"450", "Mango"},
+               {"50", "Banana"}
+               {"30", "Watermellon"},
+               {"50", "Grapes"}               
+			   
+        Output <To be printed in below format> :
+        Avaliable Fruits with price value :
+        1. Apple 200/kg
+        2. Mango 450/kg
+        3. Banana 50/kg
+
+        Avaliable Vegetables with price value :
+        1. Onion 20/kg
+        2. Potato 20/kg
+        3. Chillies 80/kg 
+	*/
+
 package com.example.demo1.model;
 
 import java.util.*;
@@ -51,31 +78,5 @@ public class MapOp {
             System.out.println(index + ". " + e.getKey() + " " + price.get(e.getKey())+"/kg");
         }
 
-
-
-//        Output <To be printed in below format> :
-//        Avaliable Fruits with price value :
-//        1. Apple 200/kg
-//        2. Mango 450/kg
-//        3. Banana 50/kg
-//
-//        Avaliable Vegetables with price value :
-//        1. Onion 20/kg
-//        2. Potato 20/kg
-//        3. Chillies 80/kg
-
-
-//
-//        map.put("XYZ",30);
-//        map.put("PQR",10);
-//        map.put("ABC",5);
-//        map.put("ABP",30);
-//        System.out.println(map);
-//
-//        TreeMap<String, Integer> stringIntegerTreeMap = new TreeMap<>(map);
-//        System.out.println(stringIntegerTreeMap);
-//        map.entrySet()
-//                .stream()
-//                .sorted(Map.Entry.comparingByKey()).forEach(System.out::println);
     }
 }
